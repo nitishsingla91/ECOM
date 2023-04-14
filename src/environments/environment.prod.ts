f93914a78110f628df5908a6b95000a4d25da3ca
Rakesh Sharma <you@example.comcls>
2023-04-14 11:41:14 +0530

    IP Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { KeycloakConfig } from "src/app/common/interfaces.defs";
 
 // Add here your keycloak setup infos
 const keycloakConfig: KeycloakConfig = {
-  url: 'http://34.71.97.225',
+  url: 'http://35.225.49.73',
   realm: 'AMCart',
   clientId: 'amcart-client',
   redirectURI: 'http://localhost:4200'
